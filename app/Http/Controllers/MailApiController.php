e0da92ba00fadbdfa81f2f39966b98ae4cd81c35
Toomas Unt <toomas.unt1968@gmail.com>
2017-03-06 22:38:09 +0200

    mail

--- app/Http/Controllers/MailApiController.php
+++ app/Http/Controllers/MailApiController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Mail\ContactMe;
 use Illuminate\Http\Request;
 
 class MailApiController extends Controller
 {
-    public function index() {
-    	return ['Toomas Unt'];
+    public function contactMail() {
+    	$myMail = 'toomas.unt1968@gmail.com';
+    	Mail::to($myMail)->send(new ContactMe);
     }
 }
