c811caf0ac1d61230e0015a8edcc03459cb1f682
Your Name <you@example.com>
2017-03-06 20:47:53 +0000

    save

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/data', 'MailApiController@index');
+Route::post('/data', 'MailApiController@index');
